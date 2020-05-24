8083d4987b3cded68ff8a3d31f5cf87db3b6b43f
asminimulin <a.s.minimulin@gmail.com>
2020-05-24 15:02:30 +0300

    Fix window's balance counting

--- src/greenhouse/window.cpp
+++ src/greenhouse/window.cpp
@@ -13,7 +13,7 @@ void Window::stepOpen(int32_t time) {
     if (balance_ + time <= fullTime_ + timeInfelicity) {
         balance_ += time;
         device_.setStateFor(OPENINIG, time);
-        balance_ = constrain(balance_, 0, fullTime_ + timeInfelicity);
+        balance_ = constrain(balance_, time, fullTime_ + timeInfelicity);
     }
 }
 
@@ -22,6 +22,6 @@ void Window::stepClose(int32_t time) {
     if (balance_ - time >= -timeInfelicity) {
         balance_ -= time;
         device_.setStateFor(CLOSING, time);
-        balance_ = constrain(balance_, -timeInfelicity, fullTime_);
+        balance_ = constrain(balance_, -timeInfelicity, fullTime_ - time);
     }
 }
