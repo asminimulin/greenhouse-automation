550dd4a87bc973c157b7b66731f8e31ebddec303
asminimulin <a.s.minimulin@gmail.com>
2020-06-10 22:38:21 +0300

    Add clang-format to the project

--- src/global_singletons.hpp
+++ src/global_singletons.hpp
@@ -1,12 +1,12 @@
 #ifndef ONE_WIRE_DRIVER_HPP
 #define ONE_WIRE_DRIVER_HPP
 
-
 // Disable unused features
 #define ONEWIRE_CRC8_TABLE 0
 #define ONEWIRE_CRC16 0
 
 #include <OneWire.h>
+
 #include <greenhouse/ds2413_driver.hpp>
 
 void initOneWire();
--- src/main.cpp
+++ src/main.cpp
@@ -2,44 +2,37 @@
 
 #include <blocker/blocker.hpp>
 #include <global_singletons.hpp>
-#include <idle.hpp>
 #include <greenhouse/greenhouse.hpp>
+#include <idle.hpp>
+
 #include "config.hpp"
+#include "connector/serial_connector.hpp"
 #include "encoder/encoder.hpp"
+#include "greenhouse/ds2413_driver.hpp"
+#include "logging/logging.hpp"
 #include "menu/menu.hpp"
 #include "menu/menu_validators.hpp"
 #include "screen/screen.hpp"
-#include "logging/logging.hpp"
-#include "connector/serial_connector.hpp"
-#include "greenhouse/ds2413_driver.hpp"
-
 
 void buildFirstGreenhouseMenu();
 void buildSecondGreenhouseMenu();
 void buildScreenMenu();
 void buildSummerModeMenu();
 
-
 Greenhouse buildFirstGreenhouse();
 Greenhouse buildSecondGreenhouse();
-
 
 Greenhouse firstGreenhouse = buildFirstGreenhouse();
 Greenhouse secondGreenhouse = buildSecondGreenhouse();
 
-
 void espHandle(Stream* stream);
 serial_connector::SerialConnector espConnector(&Serial, espHandle);
 
-
 void refreshScreen();
-
 
 void skipEspTrashWriting();
 
-
 void notifySystemBlocked();
-
 
 void setup() {
   Serial.begin(57600);
@@ -63,7 +56,6 @@ void setup() {
   firstGreenhouse.loadSettings();
   if (HAS_SECOND_GREENHOUSE) secondGreenhouse.loadSettings();
 
-
   ns_ds18b20::init();
   logging::info(F("ds18b20 is ready ready"));
 
@@ -85,7 +77,6 @@ void setup() {
   logging::info(F("Setup successfully"));
 }
 
-
 void loop() {
   if (ns_blocker::isBlocked()) {
     notifySystemBlocked();
@@ -100,75 +91,75 @@ void loop() {
   getDS2413Driver()->loop();
 }
 
-
 Greenhouse buildFirstGreenhouse() {
   GreenhouseConfig config;
-  memcpy(config.yellowSensorAddress,   FIRST_GREENHOUSE_YELLOW_SENSOR_ADDRESS, 8);
-  memcpy(config.greenSensorAddress,    FIRST_GREENHOUSE_GREEN_SENSOR_ADDRESS, 8);
-  memcpy(config.outsideSensorAddress,  FIRST_GREENHOUSE_OUTSIDE_SENSOR_ADDRESS, 8);
-  memcpy(config.yellowMotorAddress,    FIRST_GREENHOUSE_YELLOW_MOTOR_ADDRESS, 8);
-  memcpy(config.greenMotorAddress,     FIRST_GREENHOUSE_GREEN_MOTOR_ADDRESS, 8);
-  memcpy(config.ventAddress,           FIRST_GREENHOUSE_VENT_ADDRESS, 8);
+  memcpy(config.yellowSensorAddress, FIRST_GREENHOUSE_YELLOW_SENSOR_ADDRESS, 8);
+  memcpy(config.greenSensorAddress, FIRST_GREENHOUSE_GREEN_SENSOR_ADDRESS, 8);
+  memcpy(config.outsideSensorAddress, FIRST_GREENHOUSE_OUTSIDE_SENSOR_ADDRESS,
+         8);
+  memcpy(config.yellowMotorAddress, FIRST_GREENHOUSE_YELLOW_MOTOR_ADDRESS, 8);
+  memcpy(config.greenMotorAddress, FIRST_GREENHOUSE_GREEN_MOTOR_ADDRESS, 8);
+  memcpy(config.ventAddress, FIRST_GREENHOUSE_VENT_ADDRESS, 8);
   return Greenhouse(config, EEPROM_FIRST_GREENHOUSE_PROPERTIES_ADDRESS);
 }
 
-
 Greenhouse buildSecondGreenhouse() {
   GreenhouseConfig config;
-  memcpy(config.yellowSensorAddress,   SECOND_GREENHOUSE_YELLOW_SENSOR_ADDRESS, 8);
-  memcpy(config.greenSensorAddress,    SECOND_GREENHOUSE_GREEN_SENSOR_ADDRESS, 8);
-  memcpy(config.outsideSensorAddress,  SECOND_GREENHOUSE_OUTSIDE_SENSOR_ADDRESS, 8);
-  memcpy(config.yellowMotorAddress,    SECOND_GREENHOUSE_YELLOW_MOTOR_ADDRESS, 8);
-  memcpy(config.greenMotorAddress,     SECOND_GREENHOUSE_GREEN_MOTOR_ADDRESS, 8);
-  memcpy(config.ventAddress,           SECOND_GREENHOUSE_VENT_ADDRESS, 8);
+  memcpy(config.yellowSensorAddress, SECOND_GREENHOUSE_YELLOW_SENSOR_ADDRESS,
+         8);
+  memcpy(config.greenSensorAddress, SECOND_GREENHOUSE_GREEN_SENSOR_ADDRESS, 8);
+  memcpy(config.outsideSensorAddress, SECOND_GREENHOUSE_OUTSIDE_SENSOR_ADDRESS,
+         8);
+  memcpy(config.yellowMotorAddress, SECOND_GREENHOUSE_YELLOW_MOTOR_ADDRESS, 8);
+  memcpy(config.greenMotorAddress, SECOND_GREENHOUSE_GREEN_MOTOR_ADDRESS, 8);
+  memcpy(config.ventAddress, SECOND_GREENHOUSE_VENT_ADDRESS, 8);
   return Greenhouse(config, EEPROM_SECOND_GREENHOUSE_PROPERTIES_ADDRESS);
 }
-
 
 void ns_encoder::increment() {
   ns_menu::handleIncrement();
   ns_screen::resetInterruptTimer();
 }
 
-
 void ns_encoder::decrement() {
   ns_menu::handleDecrement();
   ns_screen::resetInterruptTimer();
 }
-
 
 void ns_encoder::incrementPress() {
   ns_menu::handleIncrementPress();
   ns_screen::resetInterruptTimer();
 }
 
-
 void ns_encoder::decrementPress() {
   ns_menu::handleDecrementPress();
   ns_screen::resetInterruptTimer();
 }
-
 
 void refreshScreen() {
   char buf[3][3];
   if (!ns_menu::isEnabled()) {
     char* firstRow = ns_screen::getWritableBuffer(0);
     char* secondRow = ns_screen::getWritableBuffer(1);
-    Greenhouse::getTempRepresentation(firstGreenhouse.getYellowTemperature(), buf[0]);
-    Greenhouse::getTempRepresentation(firstGreenhouse.getGreenTemperature(), buf[1]);
-    Greenhouse::getTempRepresentation(firstGreenhouse.getOutsideTemperature(), buf[2]);
+    Greenhouse::getTempRepresentation(firstGreenhouse.getYellowTemperature(),
+                                      buf[0]);
+    Greenhouse::getTempRepresentation(firstGreenhouse.getGreenTemperature(),
+                                      buf[1]);
+    Greenhouse::getTempRepresentation(firstGreenhouse.getOutsideTemperature(),
+                                      buf[2]);
     sprintf(firstRow, "Y:%s G:%s Out:%s", buf[0], buf[1], buf[2]);
-    Greenhouse::getTempRepresentation(secondGreenhouse.getYellowTemperature(), buf[0]);
-    Greenhouse::getTempRepresentation(secondGreenhouse.getGreenTemperature(), buf[1]);
-    sprintf(secondRow, "Y:%s G:%s Sum:%s", buf[0], buf[1], (firstGreenhouse.getSummerMode() ? "ON" : "OFF"));
+    Greenhouse::getTempRepresentation(secondGreenhouse.getYellowTemperature(),
+                                      buf[0]);
+    Greenhouse::getTempRepresentation(secondGreenhouse.getGreenTemperature(),
+                                      buf[1]);
+    sprintf(secondRow, "Y:%s G:%s Sum:%s", buf[0], buf[1],
+            (firstGreenhouse.getSummerMode() ? "ON" : "OFF"));
   } else {
-    char *firstRow = ns_screen::getWritableBuffer(0);
-    char *secondRow = ns_screen::getWritableBuffer(1);
+    char* firstRow = ns_screen::getWritableBuffer(0);
+    char* secondRow = ns_screen::getWritableBuffer(1);
     ns_menu::renderMenu(firstRow, secondRow);
   }
-
 }
-
 
 void buildFirstGreenhouseMenu() {
   MenuItem item;
@@ -183,7 +174,7 @@ void buildFirstGreenhouseMenu() {
   item.validator = validateFirstOpeningTemperature;
   item.isLeaf = true;
   ns_menu::addItem(item);
-  
+
   item.name = F("Closing Temp");
   item.parent = rootId;
   item.value = &firstGreenhouse.closingTemperature;
@@ -199,7 +190,6 @@ void buildFirstGreenhouseMenu() {
   ns_menu::addItem(item);
 }
 
-
 void buildSecondGreenhouseMenu() {
   MenuItem item;
   item.parent = ns_menu::getRoot();
@@ -213,7 +203,7 @@ void buildSecondGreenhouseMenu() {
   item.isLeaf = true;
   item.validator = validateSecondOpeningTemperature;
   ns_menu::addItem(item);
-  
+
   item.name = F("Closing Temp");
   item.parent = rootId;
   item.value = &secondGreenhouse.closingTemperature;
@@ -229,7 +219,6 @@ void buildSecondGreenhouseMenu() {
   ns_menu::addItem(item);
 }
 
-
 void buildScreenMenu() {
   MenuItem item;
   item.parent = ns_menu::getRoot();
@@ -241,7 +230,6 @@ void buildScreenMenu() {
   ns_menu::addItem(item);
 }
 
-
 void repsentSummerMode(int8_t value, char* buffer) {
   if ((value & 1) == 0) {
     strcpy_P(buffer, reinterpret_cast<const char*>(F("OFF")));
@@ -249,7 +237,6 @@ void repsentSummerMode(int8_t value, char* buffer) {
     strcpy_P(buffer, reinterpret_cast<const char*>(F("ON")));
   }
 }
-
 
 void buildSummerModeMenu() {
   MenuItem item;
@@ -262,17 +249,17 @@ void buildSummerModeMenu() {
   ns_menu::addItem(item);
 }
 
-
-enum EspCommandInterfaces: uint8_t {
+enum EspCommandInterfaces : uint8_t {
   COMMAND_GET_MEASURES = '1',
   COMMAND_GET_SETTINGS = '2',
 };
-enum StatusCode: uint8_t {
+enum StatusCode : uint8_t {
   OK = 0,
   ERROR = 1,
 };
 void espHandle(Stream* stream) {
-  auto command = stream->read(); // we for sure know that stream is not empty (see implementation of SerialConnector)
+  auto command = stream->read();  // we for sure know that stream is not empty
+                                  // (see implementation of SerialConnector)
   if (command == COMMAND_GET_MEASURES) {
     auto ytemp1 = static_cast<uint8_t>(firstGreenhouse.getYellowTemperature());
     auto gtemp1 = static_cast<uint8_t>(firstGreenhouse.getGreenTemperature());
@@ -287,22 +274,24 @@ void espHandle(Stream* stream) {
     stream->write(ytemp1);
     stream->write(gtemp1);
     stream->write(uint8_t(firstGreenhouse.getVentStatus()));
-    stream->write(firstGreenhouse.getYellowPerCent()); // yellow window per cent
-    stream->write(firstGreenhouse.getGreenPerCent()); // green window per cent
-    stream->write(uint8_t(0)); // blue humidity
-    stream->write(uint8_t(false)); // blue watering status
-    stream->write(uint8_t(0)); // red humidity
-    stream->write(uint8_t(false)); // red watering status
-    
+    stream->write(
+        firstGreenhouse.getYellowPerCent());           // yellow window per cent
+    stream->write(firstGreenhouse.getGreenPerCent());  // green window per cent
+    stream->write(uint8_t(0));                         // blue humidity
+    stream->write(uint8_t(false));                     // blue watering status
+    stream->write(uint8_t(0));                         // red humidity
+    stream->write(uint8_t(false));                     // red watering status
+
     stream->write(ytemp2);
     stream->write(gtemp2);
     stream->write(uint8_t(secondGreenhouse.getVentStatus()));
-    stream->write(secondGreenhouse.getYellowPerCent()); // yellow window per cent
-    stream->write(secondGreenhouse.getGreenPerCent()); // green window per cent
-    stream->write(uint8_t(0)); // blue humidity
-    stream->write(uint8_t(false)); // blue watering status
-    stream->write(uint8_t(0)); // red humidity
-    stream->write(uint8_t(false)); // red watering status
+    stream->write(
+        secondGreenhouse.getYellowPerCent());  // yellow window per cent
+    stream->write(secondGreenhouse.getGreenPerCent());  // green window per cent
+    stream->write(uint8_t(0));                          // blue humidity
+    stream->write(uint8_t(false));                      // blue watering status
+    stream->write(uint8_t(0));                          // red humidity
+    stream->write(uint8_t(false));                      // red watering status
 
   } else if (command == COMMAND_GET_SETTINGS) {
     stream->write(OK);
@@ -322,14 +311,12 @@ void espHandle(Stream* stream) {
   }
 }
 
-
 void skipEspTrashWriting() {
   delay(3000);
   while (Serial.available()) {
     Serial.read();
   }
 }
-
 
 void notifySystemBlocked() {
   logging::info(F("Testing time finished. System stopped"));
@@ -339,7 +326,7 @@ void notifySystemBlocked() {
   char* line[2] = {ns_screen::getWritableBuffer(0),
                    ns_screen::getWritableBuffer(1)};
   for (int row = 0; row < 2; ++row) {
-    for (int i = 0; ; ++i) {
+    for (int i = 0;; ++i) {
       char c = pgm_read_byte(message[row] + i);
       line[row][i] = c;
       if (c == '\0') break;
