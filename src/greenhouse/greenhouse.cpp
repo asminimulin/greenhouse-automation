99c141ebb9008fa4c4e90bbf51bca99c5d01ddba
asminimulin <a.s.minimulin@gmail.com>
2020-06-10 23:31:46 +0300

    Apply clang-format to greenhouse/greenhouse.*

--- src/greenhouse/greenhouse.cpp
+++ src/greenhouse/greenhouse.cpp
@@ -1,144 +1,153 @@
+#include "greenhouse/greenhouse.hpp"
+
 #include <EEPROM.h>
 
 #include "logging/logging.hpp"
-#include "greenhouse/greenhouse.hpp"
 
 uint8_t Greenhouse::summerMode = true;
 
-
-Greenhouse::Greenhouse(const GreenhouseConfig& config, uint16_t settingsPosition)
-    : yellowWindow_(openingTime, config.yellowMotorAddress)
-    , greenWindow_(openingTime, config.greenMotorAddress)
-    , yellowSensor(config.yellowSensorAddress)
-    , greenSensor(config.greenSensorAddress)
-    , outsideSensor(config.outsideSensorAddress)
-    , vent_(config.ventAddress) {
-        settingsPosition_ = settingsPosition;
-        openingSteps = 6;
-        openingTemperature = 24;
-        closingTemperature = 20;
+Greenhouse::Greenhouse(const GreenhouseConfig& config,
+                       uint16_t settingsPosition)
+    : yellowWindow_(openingTime, config.yellowMotorAddress),
+      greenWindow_(openingTime, config.greenMotorAddress),
+      yellowSensor(config.yellowSensorAddress),
+      greenSensor(config.greenSensorAddress),
+      outsideSensor(config.outsideSensorAddress),
+      vent_(config.ventAddress) {
+  settingsPosition_ = settingsPosition;
+  openingSteps = 6;
+  openingTemperature = 24;
+  closingTemperature = 20;
 }
-
 
 void Greenhouse::loop() {
-    summerMode &= 1;
-    yellowWindow_.loop();
-    greenWindow_.loop();
+  summerMode &= 1;
+  yellowWindow_.loop();
+  greenWindow_.loop();
 
-    int8_t yellowTemperature = yellowSensor.getTemperature();
-    int8_t greenTemperature = greenSensor.getTemperature();
-    int8_t outsideTemperature = outsideSensor.getTemperature();
-    if (yellowTemperature == -127) {
-        // logging::warning(F("Yellow sensor is dead"));
-        yellowTemperature = greenTemperature;
-    }
-    if (greenTemperature == -127) {
-        // logging::warning(F("Green sensor is dead"));
-        greenTemperature = yellowTemperature;
-    }
-    if (yellowTemperature == -127 && greenTemperature == -127) {
-        // logging::error(F("Inside sensors are dead"));
-        yellowTemperature = greenTemperature = outsideSensor.getTemperature();
-    }
-    if (outsideTemperature == -127 && yellowTemperature == -127 && greenTemperature == -127) {
-        logging::error(F("All temperature sensors are died."));
-        return;
-    }
+  int8_t yellowTemperature = yellowSensor.getTemperature();
+  int8_t greenTemperature = greenSensor.getTemperature();
+  int8_t outsideTemperature = outsideSensor.getTemperature();
+  if (yellowTemperature == -127) {
+    // logging::warning(F("Yellow sensor is dead"));
+    yellowTemperature = greenTemperature;
+  }
+  if (greenTemperature == -127) {
+    // logging::warning(F("Green sensor is dead"));
+    greenTemperature = yellowTemperature;
+  }
+  if (yellowTemperature == -127 && greenTemperature == -127) {
+    // logging::error(F("Inside sensors are dead"));
+    yellowTemperature = greenTemperature = outsideSensor.getTemperature();
+  }
+  if (outsideTemperature == -127 && yellowTemperature == -127 &&
+      greenTemperature == -127) {
+    logging::error(F("All temperature sensors are died."));
+    return;
+  }
 
-    bool shouldOpenYellowWindow = outsideSensor.getTemperature() > outsideMotorEnablingTemperature && yellowTemperature > openingTemperature;
-    shouldOpenYellowWindow |= yellowTemperature >= criticalHighTemperature;
-    shouldOpenYellowWindow &= bool(summerMode);
-    if (shouldOpenYellowWindow) {
-        if (!yellowWindow_.isBusy() && millis() - yellowWindowStateChangedAt > temperatureInnercyDelay) {
-            logging::debug(F("stepOpen yellow window"));
-            yellowWindow_.stepOpen(getOneStepTime());
-            yellowWindowStateChangedAt = millis();
-        }
+  bool shouldOpenYellowWindow =
+      outsideSensor.getTemperature() > outsideMotorEnablingTemperature &&
+      yellowTemperature > openingTemperature;
+  shouldOpenYellowWindow |= yellowTemperature >= criticalHighTemperature;
+  shouldOpenYellowWindow &= bool(summerMode);
+  if (shouldOpenYellowWindow) {
+    if (!yellowWindow_.isBusy() &&
+        millis() - yellowWindowStateChangedAt > temperatureInnercyDelay) {
+      logging::debug(F("stepOpen yellow window"));
+      yellowWindow_.stepOpen(getOneStepTime());
+      yellowWindowStateChangedAt = millis();
     }
+  }
 
-    bool shouldOpenGreenWindow = outsideSensor.getTemperature() > outsideMotorEnablingTemperature && greenTemperature > openingTemperature;
-    shouldOpenGreenWindow |= greenTemperature >= criticalHighTemperature;
-    shouldOpenGreenWindow &= bool(summerMode);
-    if (shouldOpenGreenWindow) {
-        if (!greenWindow_.isBusy() && millis() - greenWindowStateChangedAt > temperatureInnercyDelay) {
-            logging::debug(F("stepOpen green window"));
-            greenWindow_.stepOpen(getOneStepTime());
-            greenWindowStateChangedAt = millis();
-        }
+  bool shouldOpenGreenWindow =
+      outsideSensor.getTemperature() > outsideMotorEnablingTemperature &&
+      greenTemperature > openingTemperature;
+  shouldOpenGreenWindow |= greenTemperature >= criticalHighTemperature;
+  shouldOpenGreenWindow &= bool(summerMode);
+  if (shouldOpenGreenWindow) {
+    if (!greenWindow_.isBusy() &&
+        millis() - greenWindowStateChangedAt > temperatureInnercyDelay) {
+      logging::debug(F("stepOpen green window"));
+      greenWindow_.stepOpen(getOneStepTime());
+      greenWindowStateChangedAt = millis();
     }
+  }
 
-    bool shouldCloseYellowWindow = yellowTemperature < closingTemperature && yellowTemperature < criticalHighTemperature;
-    shouldCloseYellowWindow |= outsideTemperature <= criticalLowTemperature;
-    shouldCloseYellowWindow |= !bool(summerMode);
-    if (shouldCloseYellowWindow) {
-        if (!yellowWindow_.isBusy() && millis() - yellowWindowStateChangedAt > temperatureInnercyDelay) {
-            logging::debug(F("stepClose yellow window"));
-            yellowWindow_.stepClose(getOneStepTime());
-            yellowWindowStateChangedAt = millis();
-        }
+  bool shouldCloseYellowWindow = yellowTemperature < closingTemperature &&
+                                 yellowTemperature < criticalHighTemperature;
+  shouldCloseYellowWindow |= outsideTemperature <= criticalLowTemperature;
+  shouldCloseYellowWindow |= !bool(summerMode);
+  if (shouldCloseYellowWindow) {
+    if (!yellowWindow_.isBusy() &&
+        millis() - yellowWindowStateChangedAt > temperatureInnercyDelay) {
+      logging::debug(F("stepClose yellow window"));
+      yellowWindow_.stepClose(getOneStepTime());
+      yellowWindowStateChangedAt = millis();
     }
+  }
 
-    bool shouldCloseGreenWindow = greenTemperature < closingTemperature && greenTemperature < criticalHighTemperature;
-    shouldCloseGreenWindow |= outsideTemperature <= criticalLowTemperature;
-    shouldCloseGreenWindow |= !bool(summerMode);
-    if (shouldCloseGreenWindow) {
-        if (!greenWindow_.isBusy() && millis() - greenWindowStateChangedAt > temperatureInnercyDelay) {
-            logging::debug(F("stepClose green window"));
-            greenWindow_.stepClose(getOneStepTime());
-            greenWindowStateChangedAt = millis();
-        }
+  bool shouldCloseGreenWindow = greenTemperature < closingTemperature &&
+                                greenTemperature < criticalHighTemperature;
+  shouldCloseGreenWindow |= outsideTemperature <= criticalLowTemperature;
+  shouldCloseGreenWindow |= !bool(summerMode);
+  if (shouldCloseGreenWindow) {
+    if (!greenWindow_.isBusy() &&
+        millis() - greenWindowStateChangedAt > temperatureInnercyDelay) {
+      logging::debug(F("stepClose green window"));
+      greenWindow_.stepClose(getOneStepTime());
+      greenWindowStateChangedAt = millis();
     }
+  }
 
-
-    int averageTemperature = (int(yellowTemperature) + int(greenTemperature)) / 2;
-    if (averageTemperature >= ventOnTemperature) {
-        if (vent_.getState() != VentState::VENT_ON)
-            vent_.setState(VentState::VENT_ON);
-    } else if (averageTemperature <= ventOnTemperature - 2) { // NOTE: 2 is histeresis value for vent ON/OFF
-        if (vent_.getState() != VentState::VENT_OFF)
-            vent_.setState(VentState::VENT_OFF);
-    }
+  int averageTemperature = (int(yellowTemperature) + int(greenTemperature)) / 2;
+  if (averageTemperature >= ventOnTemperature) {
+    if (vent_.getState() != VentState::VENT_ON)
+      vent_.setState(VentState::VENT_ON);
+  } else if (averageTemperature <=
+             ventOnTemperature -
+                 2) {  // NOTE: 2 is histeresis value for vent ON/OFF
+    if (vent_.getState() != VentState::VENT_OFF)
+      vent_.setState(VentState::VENT_OFF);
+  }
 }
-
 
 void Greenhouse::loadSettings() {
-    auto position = settingsPosition_;
-    if (EEPROM.read(position) == HAS_VALID_DATA) {
-        logging::info(F("Loading greenhouse settings"));
-        position++;
-        EEPROM.get(position, openingTemperature);
-        position++;
-        EEPROM.get(position, closingTemperature);
-        position++;
-        EEPROM.get(position, openingSteps);
-        position += sizeof(openingSteps);
-        EEPROM.get(position, summerMode);
-    }
-    logging::info(F("Using default settings"));
+  auto position = settingsPosition_;
+  if (EEPROM.read(position) == HAS_VALID_DATA) {
+    logging::info(F("Loading greenhouse settings"));
+    position++;
+    EEPROM.get(position, openingTemperature);
+    position++;
+    EEPROM.get(position, closingTemperature);
+    position++;
+    EEPROM.get(position, openingSteps);
+    position += sizeof(openingSteps);
+    EEPROM.get(position, summerMode);
+  }
+  logging::info(F("Using default settings"));
 }
-
 
 void Greenhouse::saveSettings() {
-    logging::info(F("Save greenhouse settings"));
-    auto position = settingsPosition_;
-    EEPROM.write(position, HAS_VALID_DATA);
-    position++;
-    EEPROM.put(position, openingTemperature);
-    position++;
-    EEPROM.put(position, closingTemperature);
-    position++;
-    EEPROM.put(position, openingSteps);
-    position += sizeof(openingSteps);
-    EEPROM.put(position, summerMode);
+  logging::info(F("Save greenhouse settings"));
+  auto position = settingsPosition_;
+  EEPROM.write(position, HAS_VALID_DATA);
+  position++;
+  EEPROM.put(position, openingTemperature);
+  position++;
+  EEPROM.put(position, closingTemperature);
+  position++;
+  EEPROM.put(position, openingSteps);
+  position += sizeof(openingSteps);
+  EEPROM.put(position, summerMode);
 }
 
-
 void Greenhouse::getTempRepresentation(int8_t temperature, char* buffer) {
-    if (temperature == -127) {
-        sprintf(buffer, "--");
-    } else if (temperature == 85) {
-        sprintf(buffer, "__");
-    } else {
-        sprintf(buffer, "%d", temperature);
-    }
+  if (temperature == -127) {
+    sprintf(buffer, "--");
+  } else if (temperature == 85) {
+    sprintf(buffer, "__");
+  } else {
+    sprintf(buffer, "%d", temperature);
+  }
 }
--- src/greenhouse/greenhouse.hpp
+++ src/greenhouse/greenhouse.hpp
@@ -1,105 +1,103 @@
 #ifndef GREENHOUSE_HPP
 #define GREENHOUSE_HPP
 
+#include "config.hpp"
 #include "ds18b20.hpp"
 #include "ds2413.hpp"
-#include "config.hpp"
 #include "window.hpp"
 
-
 struct GreenhouseConfig {
-
-DeviceAddress yellowSensorAddress;
-DeviceAddress greenSensorAddress;
-DeviceAddress outsideSensorAddress;
-DeviceAddress yellowMotorAddress;
-DeviceAddress greenMotorAddress;
-DeviceAddress ventAddress;
-
+  DeviceAddress yellowSensorAddress;
+  DeviceAddress greenSensorAddress;
+  DeviceAddress outsideSensorAddress;
+  DeviceAddress yellowMotorAddress;
+  DeviceAddress greenMotorAddress;
+  DeviceAddress ventAddress;
 };
 
-
 class Greenhouse {
+  friend void buildFirstGreenhouseMenu();
+  friend void buildSecondGreenhouseMenu();
+  friend bool validateFirstOpeningTemperature(int8_t temp);
+  friend bool validateFirstClosingTemperature(int8_t temp);
+  friend bool validateFirstOpeningSteps(int8_t steps);
+  friend bool validateSecondOpeningTemperature(int8_t temp);
+  friend bool validateSecondClosingTemperature(int8_t temp);
+  friend bool validateSecondOpeningSteps(int8_t steps);
+  friend void buildSummerModeMenu();
 
-    friend void buildFirstGreenhouseMenu();
-    friend void buildSecondGreenhouseMenu();
-    friend bool validateFirstOpeningTemperature(int8_t temp);
-    friend bool validateFirstClosingTemperature(int8_t temp);
-    friend bool validateFirstOpeningSteps(int8_t steps);
-    friend bool validateSecondOpeningTemperature(int8_t temp);
-    friend bool validateSecondClosingTemperature(int8_t temp);
-    friend bool validateSecondOpeningSteps(int8_t steps);
-    friend void buildSummerModeMenu();
+ public:
+  Greenhouse(const GreenhouseConfig& config, uint16_t settingsPosition);
+  void loadSettings();
+  void saveSettings();
+  void loop();
+  static void getTempRepresentation(int8_t temperature, char* buffer);
+  int8_t getOutsideTemperature() { return outsideSensor.getTemperature(); }
+  int8_t getYellowTemperature() { return yellowSensor.getTemperature(); }
+  int8_t getGreenTemperature() { return greenSensor.getTemperature(); }
+  bool getSummerMode() const noexcept { return bool(summerMode & 1); }
+  void setSummeMode(bool enabled) noexcept { summerMode = enabled; }
+  inline uint8_t getYellowPerCent() const noexcept {
+    return yellowWindow_.getPerCent();
+  }
+  inline uint8_t getGreenPerCent() const noexcept {
+    return greenWindow_.getPerCent();
+  }
+  inline int8_t getOpeningTemperature() const noexcept {
+    return openingTemperature;
+  }
+  inline int8_t getClosingTemperature() const noexcept {
+    return closingTemperature;
+  }
+  inline uint8_t getStepsCount() const noexcept { return openingSteps; }
 
-public:
-    Greenhouse(const GreenhouseConfig& config, uint16_t settingsPosition);
-    void loadSettings();
-    void saveSettings();
-    void loop();
-    static void getTempRepresentation(int8_t temperature, char* buffer);
-    int8_t getOutsideTemperature() { return outsideSensor.getTemperature(); }
-    int8_t getYellowTemperature() { return yellowSensor.getTemperature(); }
-    int8_t getGreenTemperature() { return greenSensor.getTemperature(); }
-    bool getSummerMode() const noexcept { return bool(summerMode & 1); }
-    void setSummeMode(bool enabled) noexcept { summerMode = enabled; }
-    inline uint8_t getYellowPerCent() const noexcept { return yellowWindow_.getPerCent(); }
-    inline uint8_t getGreenPerCent() const noexcept { return greenWindow_.getPerCent(); }
-    inline int8_t getOpeningTemperature() const noexcept { return openingTemperature; }
-    inline int8_t getClosingTemperature() const noexcept { return closingTemperature; }
-    inline uint8_t getStepsCount() const noexcept { return openingSteps; }
-    
-private:
-    uint32_t getOneStepTime() const {
-        return MOTOR_OPENING_TIME / openingSteps;
-    }
+ private:
+  uint32_t getOneStepTime() const { return MOTOR_OPENING_TIME / openingSteps; }
 
-    Window yellowWindow_;
-    Window greenWindow_;
-    DS18B20 yellowSensor;
-    DS18B20 greenSensor;
-    DS18B20 outsideSensor;
-    DS2413 vent_;
+  Window yellowWindow_;
+  Window greenWindow_;
+  DS18B20 yellowSensor;
+  DS18B20 greenSensor;
+  DS18B20 outsideSensor;
+  DS2413 vent_;
 
-    // Configurable properties
-    int8_t openingTemperature = 24;
-    int8_t closingTemperature = 20;
-    uint8_t openingSteps = 6;
-    static uint8_t summerMode;
+  // Configurable properties
+  int8_t openingTemperature = 24;
+  int8_t closingTemperature = 20;
+  uint8_t openingSteps = 6;
+  static uint8_t summerMode;
 
+  // Motor
+ private:
+  uint32_t yellowWindowStateChangedAt = 0;
+  uint32_t greenWindowStateChangedAt = 0;
 
-// Motor
-private:
-    uint32_t yellowWindowStateChangedAt = 0;
-    uint32_t greenWindowStateChangedAt = 0;
+  // Unconfigurable properties
+ private:
+  static constexpr int8_t outsideMotorEnablingTemperature = 15;
+  static constexpr int8_t criticalHighTemperature = 35;
+  static constexpr int8_t criticalLowTemperature = 15;
+  static constexpr uint32_t temperatureInnercyDelay = 0.25 * 60LU * 1000LU;
+  static constexpr uint32_t openingTime = MOTOR_OPENING_TIME;
 
+  // EEPROM specific
+ private:
+  enum {
+    NO_VALID_DATA = 0,
+    HAS_VALID_DATA = 1,
+  };
+  uint16_t settingsPosition_;
 
-// Unconfigurable properties
-private:
-    static constexpr int8_t outsideMotorEnablingTemperature = 15;
-    static constexpr int8_t criticalHighTemperature = 35;
-    static constexpr int8_t criticalLowTemperature = 15;
-    static constexpr uint32_t temperatureInnercyDelay = 0.25 * 60LU * 1000LU;
-    static constexpr uint32_t openingTime = MOTOR_OPENING_TIME;
+  // Vent
+ private:
+  static constexpr int8_t ventOnTemperature = 25;
+  enum VentState {
+    VENT_ON = 0b10,
+    VENT_OFF = 0b00,
+  };
 
-// EEPROM specific
-private:
-    enum {
-        NO_VALID_DATA = 0,
-        HAS_VALID_DATA = 1,
-    };
-    uint16_t settingsPosition_;
-
-
-// Vent
-private:
-    static constexpr int8_t ventOnTemperature = 25;
-    enum VentState {
-        VENT_ON = 0b10,
-        VENT_OFF = 0b00,
-    };
-
-public:
-    inline bool getVentStatus() noexcept { return vent_.getState() == VENT_ON; }
+ public:
+  inline bool getVentStatus() noexcept { return vent_.getState() == VENT_ON; }
 };
 
 #endif
